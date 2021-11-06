f4f4d3995045d56bbf1108c990f8aa908b97ae4e
Pepe is Sad <40109798+FeelsBadMan1@users.noreply.github.com>
2021-11-06 16:17:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cloudscraper
+import pymongo
 import json
 import discord
 import os
@@ -7,29 +7,40 @@ import traceback
 import asyncio
 import datetime
 import colorama
-
+import requests
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions as MP_Error
 from discord.ext.commands import MissingRequiredArgument as MRA_Error
 from discord_slash import SlashCommand, SlashContext
 
-with open("config.json") as conf:
-    config = json.load(conf)
-    requests = cloudscraper.create_scraper()
-    prefix = config.get("Discordbot Prefix")
-    token = config.get("Discordbot Token")
-    watermark = "created by ★MoneyDrop★#2921"
 
-    # headers = {
-    #     "Authorization": "Bearer " + self.bot.get_cog("MongoDB").api_key,
-    #     "Content-Type": "application/json",
-    # }
-
-    discordbot = commands.Bot(command_prefix=prefix, case_insensitive=True, self_bot=False, intents=discord.Intents.all())
+# setup default config
+with open("app.json") as a:
+    app = json.load(a)
+    url = app["env"]["DATABASE_URL"]["description"]
+    token = app["env"]["DISCORDBOT_TOKEN"]["description"]
+    database_name = app["env"]["COLLECTION_NAME"]["description"]
+    myclient = pymongo.MongoClient(url, ssl=True, ssl_ca_certs="cacert.pem")
+    mydb = myclient[database_name]
+    collection_name = mydb["API Keys"]
+    dblist = myclient.list_database_names()
+    discordbot = commands.Bot(command_prefix="dont_put_ur_prefix_here", case_insensitive=True, self_bot=False, intents=discord.Intents.all())
     discordbot.remove_command("help")
-    slash = SlashCommand(discordbot, sync_commands=True, sync_on_cog_reload=True)
+    slash = SlashCommand(discordbot, sync_commands=True, sync_on_cog_reload=True, debug_guild=903339334017642516)
+    watermark = "created by Infinimonster#1516"
 
 
+# with open("config.json") as conf:
+#     config = json.load(conf)
+#     requests = cloudscraper.create_scraper()
+#     prefix = config.get("Discordbot Prefix")
+#     token = config.get("Discordbot Token")
+#     watermark = "created by ★MoneyDrop★#2921"
+#     discordbot = commands.Bot(command_prefix=prefix, case_insensitive=True, self_bot=False, intents=discord.Intents.all())
+#     discordbot.remove_command("help")
+#     slash = SlashCommand(discordbot, sync_commands=True, sync_on_cog_reload=True, debug_guild=123456789012345678)
+
+    # logging activities from a Member
     def Log(user, command):
         timestamp = str(datetime.datetime.now().strftime('%Y-%M-%d %H:%M:%S'))
         print(f"{colorama.Fore.RESET}"
@@ -38,7 +49,7 @@ with open("config.json") as conf:
         with open('[Data]/logs.txt', 'a', encoding='utf-8') as f: f.write(f'[{timestamp}] USER {str(user)} USED THE COMMAND {command}\n')
 
 
-    # ------------------------ LOAD COGS ------------------------ #
+    # load all cogs in the cogs folder
     if __name__ == '__main__':
         for filename in os.listdir("Cogs"):
             if filename.endswith(".py"):
